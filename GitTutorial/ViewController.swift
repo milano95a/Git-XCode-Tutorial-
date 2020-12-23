fee8c4437f6919630eedf6c53e39b02fc53ed210
abu <milano95j@gmail.com>
2020-12-22 19:19:12 -0800

    bug fixes 2

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -7,5 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController { }
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+    }
+}
 
